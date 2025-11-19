8b99d0acb96c8460702636ab110e03a72a5467aa
grappe96 <w00k0805@naver.com>
2025-11-20 01:29:44 +0900

    feat: createElement 함수 구현
    - vNode를 실제 DOM 요소로 변환하는 함수 구현
    - Falsy 값, 원시 타입, 배열, vNode 객체 처리
    - DocumentFragment를 사용한 배열 처리
    - children 재귀 처리
    - updateAttributes 함수로 속성 및 이벤트 핸들러 설정

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,61 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(String(vNode));
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+
+    vNode.forEach((child) => {
+      const childElement = createElement(child);
+
+      if (childElement) {
+        fragment.appendChild(childElement);
+      }
+    });
+
+    return fragment;
+  }
+
+  if (typeof vNode !== "object") {
+    return vNode;
+  }
+
+  if (vNode.type) {
+    const $el = document.createElement(vNode.type);
+
+    if (vNode.props) {
+      updateAttributes($el, vNode.props);
+    }
+
+    if (vNode.children && vNode.children.length > 0) {
+      vNode.children.forEach((child) => {
+        const childElement = createElement(child);
+
+        if (childElement) {
+          $el.appendChild(childElement);
+        }
+      });
+    }
+
+    return $el;
+  }
+}
+
+function updateAttributes($el, props) {
+  Object.keys(props).forEach((key) => {
+    if (key.startsWith("on")) {
+      addEvent($el, key, props[key]);
+    } else if (key === "className") {
+      $el.setAttribute("class", props[key]);
+    } else {
+      $el.setAttribute(key, props[key]);
+    }
+  });
+}
